9e3ee92157e421201f0f5f20b29e31a3a2f2d0ec
Tiago <tiagohig70@gmail.com>
2025-07-16 15:29:56 -0500

    Change in model path

--- app/controllers/iris_controller.py
+++ app/controllers/iris_controller.py
@@ -19,18 +19,22 @@ router = APIRouter()
 # Inicializar el objeto Jinja2Templates globalmente para el renderizado
 templates = Jinja2Templates(directory="app/templates")
 
-model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),  '..', 'models', 'kmeans_model.pkl')
+model_path = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)),  '..', 'models', 'model_randomf.pkl')
 
 # Cargar el modelo desde la ruta absoluta
 model = joblib.load(model_path)
 iris = load_iris()
 
+
 def predict_iris(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
     """Recibe las variables y devuelve la predicción del modelo"""
-    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    input_data = np.array(
+        [[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model.predict(input_data)
     predicted_class = iris.target_names[prediction][0]
     return predicted_class
+
 
 def generate_plot(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
     """Genera una matriz de dispersión usando seaborn y marca el punto de entrada del usuario en todas las gráficas"""
@@ -40,11 +44,13 @@ def generate_plot(sepal_length: float, sepal_width: float, petal_length: float, 
 
     # Crear la matriz de dispersión con seaborn
     sns.set(style="ticks")
-    g = sns.pairplot(df, hue="species", markers=["o", "s", "D"], palette="husl")
+    g = sns.pairplot(df, hue="species", markers=[
+                     "o", "s", "D"], palette="husl")
 
     # Agregar el punto de entrada del usuario en todas las gráficas
     for ax in g.axes.flatten():
-        ax.scatter(sepal_length, sepal_width, color='black', label="Entrada del Usuario", edgecolors='white', s=100, zorder=5)
+        ax.scatter(sepal_length, sepal_width, color='black',
+                   label="Entrada del Usuario", edgecolors='white', s=100, zorder=5)
 
     # Leyenda
     handles, labels = ax.get_legend_handles_labels()
@@ -56,27 +62,31 @@ def generate_plot(sepal_length: float, sepal_width: float, petal_length: float, 
     buf.seek(0)
     img = Image.open(buf)
     img_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
-    
+
     # Cerrar la figura para liberar memoria
     plt.close(g.figure)
 
     return img_base64
+
 
 @router.get("/", response_class=HTMLResponse)
 async def form(request: Request):
     """Renderiza el formulario donde el usuario puede ingresar datos"""
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @router.post("/predict", response_class=HTMLResponse)
 async def predict(request: Request, sepal_length: float = Form(...), sepal_width: float = Form(...),
                   petal_length: float = Form(...), petal_width: float = Form(...)):
     """Recibe las variables y devuelve la predicción y gráfico"""
-    
+
     # Realizar la predicción usando el controlador
-    predicted_class = predict_iris(sepal_length, sepal_width, petal_length, petal_width)
+    predicted_class = predict_iris(
+        sepal_length, sepal_width, petal_length, petal_width)
 
     # Generar la gráfica usando el controlador
-    img_base64 = generate_plot(sepal_length, sepal_width, petal_length, petal_width)
+    img_base64 = generate_plot(
+        sepal_length, sepal_width, petal_length, petal_width)
 
     # Renderizar resultados en HTML
     return templates.TemplateResponse("result.html", {"request": request, "prediction": predicted_class, "image_base64": img_base64})
